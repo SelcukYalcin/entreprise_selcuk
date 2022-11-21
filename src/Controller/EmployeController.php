eca63476795f05fa529c32d0ff7273cadf4e560b
SelcukYalcin <98817652+SelcukYalcin@users.noreply.github.com>
2022-11-21 17:00:18 +0100

    creation entreprise

---
+++ src/Controller/EmployeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use doctrine;
+use App\Entity\Employe;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class EmployeController extends AbstractController
+{
+    /**
+     * @Route("/employe", name="app_employe")
+     */
+    public function index(ManagerRegistry $doctrine): Response
+    {
+        $employes = $doctrine->getRepository(Employe::class)->findAll();
+
+        return $this->render('employe/index.html.twig', [
+            'employes' => $employes
+
+        ]);
+    }
+}
