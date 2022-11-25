bafe9ef2d2e67f16bb2f5ed1314814d429c616b7
SelcukYalcin <yalcinselcuk79@gmail.com>
2022-11-25 01:18:00 +0100

    ajout employé

--- src/Controller/EmployeController.php
+++ src/Controller/EmployeController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use doctrine;
 use App\Entity\Employe;
+use App\Form\EmployeType;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -24,7 +26,34 @@ class EmployeController extends AbstractController
         ]);
     }
 
-        /**
+
+    /**
+     * @Route("/employe/add", name="add_employe")
+     */
+
+    public function add(ManagerRegistry $doctrine, Employe $employe = null, Request $request): Response
+    {
+        $form = $this->createForm(EmployeType::class, $employe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $employe = $form->getData();
+            $entityManager = $doctrine->getManager();
+            //prepare
+            $entityManager->persist($employe);
+            //insert into (execute)
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_employe');
+        }
+
+        // Vue pour afficher le formulaire d'ajout
+        return $this->render('employe/add.html.twig', [
+            'formAddEmploye' => $form->createView()
+        ]);
+    }
+    /**
      * @Route("/employe/{id}", name="show_employe")
      */
     public function show(Employe $employe): Response
--- src/Form/EmployeType.php
+++ src/Form/EmployeType.php
@@ -3,10 +3,14 @@
 namespace App\Form;
 
 use App\Entity\Employe;
-use Doctrine\DBAL\Types\TextType;
+use App\Entity\Entreprise;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class EmployeType extends AbstractType
 {
@@ -15,10 +19,11 @@ class EmployeType extends AbstractType
         $builder
             ->add('nom', TextType::class)
             ->add('prenom', TextType::class)
-            ->add('dateNaissance', TextType::class)
-            ->add('dateEmbauche', TextType::class)
+            ->add('dateNaissance', DateType::class, ['widget' => 'single_text'])
+            ->add('dateEmbauche', DateType::class, ['widget' => 'single_text'])
             ->add('ville', TextType::class)
-            ->add('entreprise');
+            ->add('entreprise', EntityType::class, ['class' => Entreprise::class])
+            ->add('submit', SubmitType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
